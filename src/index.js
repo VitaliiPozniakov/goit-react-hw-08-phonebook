1b5288bd88ce6ec6d148e1ef16e1d8fe307bac46
VitaliiPozniakov <vertalen89@gmail.com>
2022-11-04 19:03:24 +0200

    add basename to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-   <BrowserRouter>
+   <BrowserRouter basename="/goit-react-hw-08-phonebook/">
    <PersistGate loading={<Loader />} persistor={persistor}>
    <App />
    </PersistGate>
